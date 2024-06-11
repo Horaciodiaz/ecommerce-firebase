d97f1eda03ced42a3f244f5e070a9bc18459019a
Horacio Diaz <horacio.diaz.2000@gmail.com>
2024-06-11 12:45:23 -0300

    para despliegue de prueba

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -8,12 +8,12 @@ import { trigger, style, animate, transition } from '@angular/animations';
   animations: [
     trigger('carouselAnimation', [
       transition(':enter', [
-        style({ transform: 'translateX(-100%)', opacity: 0 }),
-        animate('800ms ease-in-out', style({ transform: 'translateX(0%)', opacity: 1 }))
+        style({ transform: 'translateX(0%)', opacity: 0 }),
+        animate('10ms ease-in', style({ transform: 'translateX(0%)', opacity: 1 }))
       ]),
       transition(':leave', [
         style({ transform: 'translateX(0%)', opacity: 1 }),
-        animate('800ms ease-in-out', style({ transform: 'translateX(100%)', opacity: 0 }))
+        animate('1000ms ease-in-out', style({ transform: 'translateX(0%)', opacity: 0 }))
       ])
     ])
   ]
