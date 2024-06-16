acbf2fc93cefea38c0fc15a341670e59d70bde60
Horacio Diaz <horacio.diaz.2000@gmail.com>
2024-06-16 03:10:34 -0300

    responsive home page

--- src/app/clients/components/quick-contact/quick-contact.component.ts
+++ src/app/clients/components/quick-contact/quick-contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-quick-contact',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./quick-contact.component.css']
 })
 export class QuickContactComponent {
+  constructor(private renderer: Renderer2) {}
 
+  ngOnInit() {
+    this.checkScreenSize();
+    window.addEventListener('resize', this.checkScreenSize.bind(this));
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.checkScreenSize.bind(this));
+  }
+
+  checkScreenSize() {
+    const element = document.getElementById('quick-contact');
+    if (window.innerWidth <= 480) {
+      this.renderer.addClass(element, 'snap-section');
+    } else {
+      this.renderer.removeClass(element, 'snap-section');
+    }
+  }
 }
--- src/app/clients/components/testimonios/testimonios.component.ts
+++ src/app/clients/components/testimonios/testimonios.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-testimonios',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./testimonios.component.css']
 })
 export class TestimoniosComponent {
+  constructor(private renderer: Renderer2) {}
 
+  ngOnInit() {
+    this.checkScreenSize();
+    window.addEventListener('resize', this.checkScreenSize.bind(this));
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.checkScreenSize.bind(this));
+  }
+
+  checkScreenSize() {
+    const element = document.getElementById('testimonials');
+    if (window.innerWidth <= 480) {
+      this.renderer.addClass(element, 'snap-section');
+    } else {
+      this.renderer.removeClass(element, 'snap-section');
+    }
+  }
 }
